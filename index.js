8c0e06c5fd060e0a928dd54a65c8f740545cdb1c
Mercy Atiemo <mercyatiemo@gmail.com>
2020-02-21 14:27:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ function logWhisper(string) {console.log(string.toLowerCase())}
 function sayHiToGrandma(string) 
 {if (string.toLowerCase() === string) {return "I can't hear you!"}
 if (string.toUpperCase() === string) {return "YES INDEED!"}
-if (string === "I love you") {return "I love you too"}}
+if (string === "I love you") {return "I love you, too."}}
